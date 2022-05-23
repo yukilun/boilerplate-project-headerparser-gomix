313877349d465e7bec743f69bd06451188e20cae
yukilun <102889935+yukilun@users.noreply.github.com>
2022-05-23 00:21:38 +0000

    1st Attempt

--- index.js
+++ index.js
@@ -25,6 +25,13 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
+app.get('/api/whoami', (req, res)=>{
+  res.json({
+    ipaddress: req.ip,
+    language: req.get('accept-language'),
+    software: req.get('user-agent')
+  });
+})
 
 
 // listen for requests :)
